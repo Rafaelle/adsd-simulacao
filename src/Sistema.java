1bfaeafbad21e94dbc99a69feaf06429f5e5c93d
alineatc <aline.costa.cc@gmail.com>
2017-07-14 00:30:31 -0300

    Adiciona saída da execução em um arquivo de texto

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,4 +1,7 @@
-import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Sistema {
 	
@@ -7,8 +10,13 @@ public class Sistema {
 	private static Servidor servidor;
 	private int proximaChegadaF1;
 	private int proximaChegadaF2;
+	
+	private static File file;
+	private static BufferedWriter writer;
+	
+	private static final String caminho_arquivo = "saida_execucao.txt";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 	
 		int tempoSimulacao = 10;
 		int tempoDecorrido = 0;
@@ -20,7 +28,11 @@ public class Sistema {
 		fila2.setProximaChegada(tempoDecorrido);
 
 		//Servidor
-		servidor = new Servidor(3,7);	
+		servidor = new Servidor(3,7);
+		
+		//Escrita da saída
+		file = new File(caminho_arquivo);
+		writer = new BufferedWriter(new FileWriter(file));
 		
 		while(tempoDecorrido <= tempoSimulacao){
 			tempoDecorrido +=1;	
@@ -36,13 +48,13 @@ public class Sistema {
 				
 				fila1.setProximaChegada(tempoDecorrido);
 				fila1.addCliente(tempoDecorrido);
-				printEvento("Chegada", tempoDecorrido, 1);
+				escreveNoArquivo("Chegada", tempoDecorrido, 1);
 				
 				if(servidor.livre()){
 					// O tempo de 'término de serviço' é calculado dentro de atender()
 					servidor.atender(tempoDecorrido);
 					fila1.atendeCliente();
-					printEvento("Saida", tempoDecorrido, 1);
+					escreveNoArquivo("Saida", tempoDecorrido, 1);
 				}
 			} 
 			
@@ -56,12 +68,12 @@ public class Sistema {
 			if (fila2.getProximaChegada() == tempoDecorrido){
 				fila2.setProximaChegada(tempoDecorrido);
 				fila2.addCliente(tempoDecorrido);
-				printEvento("Chegada", tempoDecorrido, 2);
+				escreveNoArquivo("Chegada", tempoDecorrido, 2);
 				if(servidor.livre() && fila1.getQtdClientes()==0){
 					// O tempo de 'término de serviço' é calculado dentro de atender()
 					servidor.atender(tempoDecorrido);
 					fila2.atendeCliente();
-					printEvento("Saida", tempoDecorrido, 2);
+					escreveNoArquivo("Saida", tempoDecorrido, 2);
 				}
 			}
 			
@@ -72,7 +84,6 @@ public class Sistema {
 						Remova freguês da fila	
 						Escalone evento “Término de serviço” (randômico entre 3 e 7)
 			*/
-			
 			if(servidor.getTempoServico() == tempoDecorrido){
 				if(fila1.getQtdClientes()==0 && fila2.getQtdClientes()==0){
 					servidor.teminarAtendimento();
@@ -80,40 +91,58 @@ public class Sistema {
 					// O tempo de 'término de serviço' é calculado dentro de atender()
 					servidor.atender(tempoDecorrido);
 					fila1.atendeCliente();
-					printEvento("Saída", tempoDecorrido, 1);
+					escreveNoArquivo("Saída", tempoDecorrido, 1);
 				} else if(fila2.getQtdClientes() > 0){
 					// O tempo de 'término de serviço' é calculado dentro de atender()
 					servidor.atender(tempoDecorrido);
 					fila2.atendeCliente();
-					printEvento("Saída", tempoDecorrido, 2);
+					escreveNoArquivo("Saída", tempoDecorrido, 2);
 				}
 			}
 			
 			if(servidor.livre()){
 				if(fila1.getQtdClientes() > 0){
 					servidor.atender(tempoDecorrido);
-					printEvento("Saída", tempoDecorrido, 1); // se der algum erro na execução, inverter essa linha com a de baixo.
+					escreveNoArquivo("Saída", tempoDecorrido, 1); // se der algum erro na execução, inverter essa linha com a de baixo.
 					fila1.atendeCliente();
 				} else if(fila2.getQtdClientes() > 0){
 					servidor.atender(tempoDecorrido);
-					printEvento("Saída", tempoDecorrido, 2); // se der algum erro na execução, inverter essa linha com a de baixo.
+					escreveNoArquivo("Saída", tempoDecorrido, 2); // se der algum erro na execução, inverter essa linha com a de baixo.
 					fila2.atendeCliente();
 				}	
 			} 
 		}
+		
+		encerraEscrita();
 	}
 	
-	private static void printEvento(String evento, int tempoDecorrido, int elementoNoServico){
+	// Escreve a saída no arquivo saida_execucao.txt.
+	private static void escreveNoArquivo(String evento, int tempoDecorrido, int elementoNoServico) throws IOException{
 		/*Tipo de evento: Chegada/Saída, Momento do evento: Z
 		Elementos na Fila 1: X
 		Elementos na Fila 2: Y
 		Elemento no serviço: A
 	*/
-		System.out.println("Tipo de evento: " + evento +", Momento do evento: " + tempoDecorrido);
-		System.out.println("Elementos na Fila 1: " + fila1.getQtdClientes());
-		System.out.println("Elementos na Fila 2: " + fila2.getQtdClientes());
-		// TODO Não entendi esse
-		System.out.println("Elemento no serviço: " + elementoNoServico);
-		System.out.println("------------------------------------------------------" );
+		
+		writer.write("Tipo de evento: " + evento +", Momento do evento: " + tempoDecorrido);
+		writer.newLine();
+		writer.write("Elementos na Fila 1: " + fila1.getQtdClientes());
+		writer.newLine();
+		writer.write("Elementos na Fila 2: " + fila2.getQtdClientes());
+		
+		writer.newLine();
+		writer.write("Elemento no serviço: " + elementoNoServico);
+		writer.newLine();
+		writer.write("------------------------------------------------------" );
+		writer.newLine();
+		
+		//Criando o conteúdo do arquivo
+		writer.flush();
 	}
+	
+	private static void encerraEscrita() throws IOException {
+		//Fechando conexão e escrita do arquivo.
+		writer.close();
+	}
+	
 }
